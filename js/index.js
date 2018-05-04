86bea62ff6e7c4f3f6310199bd31993bdcab5926
Julia <ami_mister@mail.ru>
2018-05-04 11:54:31 +0300

    webpack and eslint config

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const sample = 'I read book';
+function show(some) {
+  return some;
+}
+show(sample);
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'none', /* production | development | none */
+  entry: './js/index.js', /* string | object | array - entering files */
+  output: {
+    filename: 'bundle.js',
+    publicPath: '/',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [{
+      test: /\.js$/, /* check all js files */
+      exclude: /node_modules/, /* ignore all files in folder node_modules */
+      enforce: 'pre', /* loader | pre | post */
+      use: 'eslint-loader',
+    },
+    {
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: { /* can be array for several loaders */
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015'], /* end dd presets in .babelrc */
+        },
+      },
+    }],
+  },
+};
